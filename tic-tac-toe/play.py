6930d111c2494d4083dbfd5d6e284cdd03a17ec0
jcasasus5 <jcasasus.ieu2020@student.ie.edu>
2023-06-19 14:18:13 +0200

    commenting code

--- tic-tac-toe/play.py
+++ tic-tac-toe/play.py
@@ -1,5 +1,8 @@
 from utils.main_functions import main_loop, get_coordinates
 
 if __name__ == "__main__": 
+
+    # To establish the playing area in the game, begin by clicking on all four corners of the screen using your mouse. 
+    # Then the game will start.
     board = get_coordinates()
     game = main_loop(board)
